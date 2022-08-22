fb5d9c552ee817e3fb3d00e3f4d9b25c19911e06
Ankitsingh6299 <ankitsingh6299@gmail.com>
2022-08-22 16:40:41 +0530

    Addedjs and isCollide function

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let inputDir ={x:0,y:0}
+const foodSound = new Audio('music/food.mp3')
+const gameOverSound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
+let speed=19
+let score=0;
+let lastPaintTime=0;
+let snakeArr=[{
+    x:13,y:15
+}]
+food={x:6,y:7}
+
+function main(ctime){
+    window.requestAnimationFrame(main);
+    if((ctime-lastPaintTime)/1000 < 1/speed){
+        return
+    }
+    lastPaintTime=ctime
+    gameEngine();
+}
+function isCollide(snake){
+    for(let i=1;i<snakeArr.length;i++){
+     if(snake[i].x===snake[0].x && snake[i].y===snake[0].y){
+        return true;
+     }   
+    }
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        return true;
+    }
+        
+    return false;
+
+}
