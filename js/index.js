361c2c40d00f929e79cb0b887e019d260f7710e5
Ankitsingh6299 <ankitsingh6299@gmail.com>
2022-08-24 01:16:14 +0530

    Minor changes done

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ food = {x: 6, y: 7};
 // Game Functions
 function main(ctime) {
     window.requestAnimationFrame(main);
-    // console.log(ctime)
+    
     if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
     }
@@ -25,13 +25,13 @@ function main(ctime) {
 }
 
 function isCollide(snake) {
-    // If you bump into yourself 
+    
     for (let i = 1; i < snakeArr.length; i++) {
         if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
             return true;
         }
     }
-    // If you bump into the wall
+ 
     if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
         return true;
     }
@@ -40,7 +40,7 @@ function isCollide(snake) {
 }
 
 function gameEngine(){
-    // Part 1: Updating the snake array & Food
+   
     if(isCollide(snakeArr)){
         gameOverSound.play();
         musicSound.pause();
@@ -51,7 +51,7 @@ function gameEngine(){
         score = 0; 
     }
 
-    // If you have eaten the food, increment the score and regenerate the food
+    // eaten the food, increment the score and regenerate the food
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
         foodSound.play();
         score += 1;
@@ -75,7 +75,7 @@ function gameEngine(){
     snakeArr[0].x += inputDir.x;
     snakeArr[0].y += inputDir.y;
 
-    // Part 2: Display the snake and Food
+
     // Display the snake
     board.innerHTML = "";
     snakeArr.forEach((e, index)=>{
@@ -102,7 +102,7 @@ function gameEngine(){
 }
 
 
-// Main logic starts here
+
 musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
 if(hiscore === null){
